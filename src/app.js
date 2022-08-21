f0526dc0acdbcfc50cc23e9474cf8a67b9a9dd75
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2022-08-21 06:27:54 +0300

    Added the relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -23,21 +23,26 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement=document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     tempElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+    
 }    
 
+ 
 let apiKey = "95a0279efa62b6e7d0fd1891ab7005da";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=95a0279efa62b6e7d0fd1891ab7005da&units=metric";
+let city = "Kisumu";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
 
 axios.get(apiUrl).then(displayTemperature);
-
 
 
 
